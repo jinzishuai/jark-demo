5b830f5b119225fc743fe9766155d6fccb28ddca
Shi Jin <jinzishuai@gmail.com>
2024-01-15 19:49:42 -0700

    Notes for Install Karpenter 0.33.0 on an Existing EKS-1.27

---
+++ install-karpenter-2-eks/install-karpenter-2-eks-step1.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+
+export KARPENTER_VERSION=v0.33.0
+export K8S_VERSION=1.28
+
+export AWS_PARTITION="aws" # if you are not using standard partitions, you may need to configure to aws-cn / aws-us-gov
+export CLUSTER_NAME="jark-stack"
+export AWS_DEFAULT_REGION="us-east-1"
+export AWS_REGION="$(aws configure list | grep region | tr -s " " | cut -d" " -f3)"
+# might need to set AWS_PROFILE
+export AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
+export OIDC_ENDPOINT="$(aws eks describe-cluster --name ${CLUSTER_NAME} \
+    --query "cluster.identity.oidc.issuer" --output text)"
+
+# step 1: create IAM roles
+
+echo '{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}' > node-trust-policy.json
+
+aws iam create-role --role-name "KarpenterNodeRole-${CLUSTER_NAME}" \
+    --assume-role-policy-document file://node-trust-policy.json
+
+# 2. attach policies
+
+aws iam attach-role-policy --role-name "KarpenterNodeRole-${CLUSTER_NAME}" \
+    --policy-arn arn:${AWS_PARTITION}:iam::aws:policy/AmazonEKSWorkerNodePolicy
+
+aws iam attach-role-policy --role-name "KarpenterNodeRole-${CLUSTER_NAME}" \
+    --policy-arn arn:${AWS_PARTITION}:iam::aws:policy/AmazonEKS_CNI_Policy
+
+aws iam attach-role-policy --role-name "KarpenterNodeRole-${CLUSTER_NAME}" \
+    --policy-arn arn:${AWS_PARTITION}:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly
+
+aws iam attach-role-policy --role-name "KarpenterNodeRole-${CLUSTER_NAME}" \
+    --policy-arn arn:${AWS_PARTITION}:iam::aws:policy/AmazonSSMManagedInstanceCore
+
+# 3. create instance profile
+
+aws iam create-instance-profile \
+   --instance-profile-name KarpenterNodeInstanceProfile-${CLUSTER_NAME}
+
+aws iam add-role-to-instance-profile \
+   --instance-profile-name KarpenterNodeInstanceProfile-${CLUSTER_NAME} \
+   --role-name KarpenterNodeRole-${CLUSTER_NAME}
+   
+# 4. Karpenter control role
+
+cat << EOF > controller-trust-policy.json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Federated": "arn:${AWS_PARTITION}:iam::${AWS_ACCOUNT_ID}:oidc-provider/${OIDC_ENDPOINT#*//}"
+            },
+            "Action": "sts:AssumeRoleWithWebIdentity",
+            "Condition": {
+                "StringEquals": {
+                    "${OIDC_ENDPOINT#*//}:aud": "sts.amazonaws.com",
+                    "${OIDC_ENDPOINT#*//}:sub": "system:serviceaccount:karpenter:karpenter"
+                }
+            }
+        }
+    ]
+}
+EOF
+
+aws iam create-role --role-name KarpenterControllerRole-${CLUSTER_NAME} \
+    --assume-role-policy-document file://controller-trust-policy.json
+
+cat << EOF > controller-policy.json
+{
+    "Statement": [
+        {
+            "Action": [
+                "ssm:GetParameter",
+                "ec2:DescribeImages",
+                "ec2:RunInstances",
+                "ec2:DescribeSubnets",
+                "ec2:DescribeSecurityGroups",
+                "ec2:DescribeLaunchTemplates",
+                "ec2:DescribeInstances",
+                "ec2:DescribeInstanceTypes",
+                "ec2:DescribeInstanceTypeOfferings",
+                "ec2:DescribeAvailabilityZones",
+                "ec2:DeleteLaunchTemplate",
+                "ec2:CreateTags",
+                "ec2:CreateLaunchTemplate",
+                "ec2:CreateFleet",
+                "ec2:DescribeSpotPriceHistory",
+                "pricing:GetProducts"
+            ],
+            "Effect": "Allow",
+            "Resource": "*",
+            "Sid": "Karpenter"
+        },
+        {
+            "Action": "ec2:TerminateInstances",
+            "Condition": {
+                "StringLike": {
+                    "ec2:ResourceTag/karpenter.sh/nodepool": "*"
+                }
+            },
+            "Effect": "Allow",
+            "Resource": "*",
+            "Sid": "ConditionalEC2Termination"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "iam:PassRole",
+            "Resource": "arn:${AWS_PARTITION}:iam::${AWS_ACCOUNT_ID}:role/KarpenterNodeRole-${CLUSTER_NAME}",
+            "Sid": "PassNodeIAMRole"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "eks:DescribeCluster",
+            "Resource": "arn:${AWS_PARTITION}:eks:${AWS_REGION}:${AWS_ACCOUNT_ID}:cluster/${CLUSTER_NAME}",
+            "Sid": "EKSClusterEndpointLookup"
+        }
+    ],
+    "Version": "2012-10-17"
+}
+EOF
+
+aws iam put-role-policy --role-name KarpenterControllerRole-${CLUSTER_NAME} \
+    --policy-name KarpenterControllerPolicy-${CLUSTER_NAME} \
+    --policy-document file://controller-policy.json
+
+# 6. tag subnets 
+
+for NODEGROUP in $(aws eks list-nodegroups --cluster-name ${CLUSTER_NAME} \
+    --query 'nodegroups' --output text); do aws ec2 create-tags \
+        --tags "Key=karpenter.sh/discovery,Value=${CLUSTER_NAME}" \
+        --resources $(aws eks describe-nodegroup --cluster-name ${CLUSTER_NAME} \
+        --nodegroup-name $NODEGROUP --query 'nodegroup.subnets' --output text )
+done
+
+# 7. tag NSGs (first nodegroup only)
+
+NODEGROUP=$(aws eks list-nodegroups --cluster-name ${CLUSTER_NAME} \
+    --query 'nodegroups[0]' --output text)
+
+LAUNCH_TEMPLATE=$(aws eks describe-nodegroup --cluster-name ${CLUSTER_NAME} \
+    --nodegroup-name ${NODEGROUP} --query 'nodegroup.launchTemplate.{id:id,version:version}' \
+    --output text | tr -s "\t" ",")
+
+# # If your EKS setup is configured to use only Cluster security group, then please execute -
+
+# SECURITY_GROUPS=$(aws eks describe-cluster \
+#     --name ${CLUSTER_NAME} --query "cluster.resourcesVpcConfig.clusterSecurityGroupId" --output text)
+
+# If your setup uses the security groups in the Launch template of a managed node group, then :
+
+SECURITY_GROUPS=$(aws ec2 describe-launch-template-versions \
+    --launch-template-id ${LAUNCH_TEMPLATE%,*} --versions ${LAUNCH_TEMPLATE#*,} \
+    --query 'LaunchTemplateVersions[0].LaunchTemplateData.[NetworkInterfaces[0].Groups||SecurityGroupIds]' \
+    --output text)
+
+aws ec2 create-tags \
+    --tags "Key=karpenter.sh/discovery,Value=${CLUSTER_NAME}" \
+    --resources ${SECURITY_GROUPS}
---
+++ install-karpenter-2-eks/install-node-pool-default.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# ref: https://karpenter.sh/docs/getting-started/getting-started-with-karpenter/#5-create-nodepool
+
+export CLUSTER_NAME="jark-stack"
+
+cat <<EOF | envsubst | kubectl apply -f -
+apiVersion: karpenter.sh/v1beta1
+kind: NodePool
+metadata:
+  name: default
+spec:
+  template:
+    spec:
+      requirements:
+        - key: kubernetes.io/arch
+          operator: In
+          values: ["amd64"]
+        - key: kubernetes.io/os
+          operator: In
+          values: ["linux"]
+        - key: karpenter.sh/capacity-type
+          operator: In
+          values: ["on-demand"]
+        - key: capacity-spread
+          operator: In
+          values:
+          - "1"
+        - key: karpenter.k8s.aws/instance-category
+          operator: In
+          values: ["c", "m", "r"]
+        - key: karpenter.k8s.aws/instance-generation
+          operator: Gt
+          values: ["2"]
+      nodeClassRef:
+        name: default
+  limits:
+    cpu: 1000
+  disruption:
+    consolidationPolicy: WhenUnderutilized
+    expireAfter: 720h # 30 * 24h = 720h
+---
+apiVersion: karpenter.k8s.aws/v1beta1
+kind: EC2NodeClass
+metadata:
+  name: default
+spec:
+  amiFamily: AL2 # Amazon Linux 2
+  role: "KarpenterNodeRole-${CLUSTER_NAME}" # replace with your cluster name
+  subnetSelectorTerms:
+    - tags:
+        karpenter.sh/discovery: "${CLUSTER_NAME}" # replace with your cluster name
+  securityGroupSelectorTerms:
+    - tags:
+        karpenter.sh/discovery: "${CLUSTER_NAME}" # replace with your cluster name
+  tags:                  
+    "cwi:team": devops
+    # Optional, configures storage devices for the instance
+  blockDeviceMappings:
+    - deviceName: /dev/xvda
+      ebs:
+        volumeSize: 100Gi
+        volumeType: gp3
+EOF
