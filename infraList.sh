136c008a7df8709eaf0c51b42e42eff4c4305993
Shi Jin <jinzishuai@gmail.com>
2024-01-18 20:12:54 -0700

    add listing of KMS and CloudWatch Log Group

--- infraList.sh
+++ infraList.sh
@@ -11,9 +11,18 @@ echo "list of EFS ..."
 aws efs describe-file-systems
 
 echo
-echo "list of VPC ..."
+echo "list of jark* VPC ..."
 aws ec2 describe-vpcs --filters "Name=tag:Name,Values=jark*"
 
 echo
 echo "list of Karpenter IAM roles ..."
 aws iam list-roles --query "Roles[?starts_with(RoleName,'Karpenter')]"
+
+
+echo 
+echo "list of jark alias KMS keys ..."
+aws kms list-aliases --query 'Aliases[?contains(AliasName, `jark`)]'
+
+echo 
+echo "list of jark CloudWatch Logs log group ..."
+aws logs describe-log-groups --query "logGroups[?contains(logGroupName, 'jark')]"
